56a8a5ed401bd4d3c02e9647344709cf7e841d25
medina5 <emmanuel.medina@univ-lorraine.fr>
2025-06-09 20:25:18 +0200

    dashboard auth

--- start/routes.ts
+++ start/routes.ts
@@ -8,5 +8,11 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
 
 router.on('/').render('pages/home')
+
+router.get('/dashboard', async ({ view }) => view.render('pages/dashboard')) 
+  .use([
+    middleware.auth()
+  ])
